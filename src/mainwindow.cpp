b2a1bc590aa1b3c81dbb87380efa6068f23a01f6
Paul WOISARD <paulwoisard@gmail.com>
2022-02-16 19:55:06 +0100

    Début pour rendre le menu crossplatform

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,16 +1,13 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QDesktopWidget>
-#include <string>
 #include <QWindow>
 #include <QDockWidget>
 #include <QTreeWidget>
 #include <map>
 #include <QGuiApplication>
 #include <QObject>
-#include <QPalette>
-
-std::map<std::string, QString> QStringMap;
+#include <QtSvg>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -68,52 +65,90 @@ void MainWindow::actionQuitter_activate()
 
 void MainWindow::on_InstallationHeficienceMenu_clicked()
 {
+#ifdef __linux__
     cmdScript = "x-terminal-emulator --nofork -e bash -c \"wget https://github.com/Heficience/heficience-scripts/raw/main/linux/install_heficience-menu.sh ; sleep 3 ; bash ./install_heficience-menu.sh ; sleep 3 ; rm ./install_heficience-menu.sh ; sleep 3\"";
+#elif _WIN32
+    cmdScript = "";
+#elif __APPLE__
+    cmdScript = "";
+#endif
     installApp(*ui->InstallationHeficienceMenu, *ui->DesinstallationHeficienceMenu, cmdScript,
                "background-color: #22577a");
 }
 
 void MainWindow::on_DesinstallationHeficienceMenu_clicked()
 {
+#ifdef __linux__
     cmdScript = "x-terminal-emulator --nofork -e bash -c \"wget https://github.com/Heficience/heficience-scripts/raw/main/linux/uninstall_heficience-menu.sh ; sleep 3 ; bash ./uninstall_heficience-menu.sh ; sleep 3 ; rm ./uninstall_heficience-menu.sh sleep 3\"";
+#elif _WIN32
+    cmdScript = "";
+#elif __APPLE__
+    cmdScript = "";
+#endif
     installApp(*ui->DesinstallationHeficienceMenu, *ui->InstallationHeficienceMenu, cmdScript,
                "background-color: #22577a");
 }
 void MainWindow::on_InstallationAutocompletion_clicked()
 {
+#ifdef __linux__
     cmdScript = "x-terminal-emulator --nofork -e bash -c \"wget https://github.com/Heficience/heficience-scripts/raw/main/linux/install_autocompletion.sh ; sleep 3 ; bash ./install_autocompletion.sh ; sleep 3 ; rm ./install_autocompletion.sh ; sleep 3\"";
+#elif _WIN32
+    cmdScript = "";
+#elif __APPLE__
+    cmdScript = "";
+#endif
     installApp(*ui->InstallationAutocompletion, *ui->DesinstallationAutocompletion, cmdScript,
                "background-color: #38a3a5; color: #000");
 }
 
 void MainWindow::on_DesinstallationAutocompletion_clicked()
 {
+#ifdef __linux__
     cmdScript = "x-terminal-emulator --nofork -e bash -c \"wget https://github.com/Heficience/heficience-scripts/raw/main/linux/uninstall_autocompletion.sh ; sleep 3 ; bash ./uninstall_autocompletion.sh ; sleep 3 ; rm ./uninstall_autocompletion.sh ; rm -rf $HOME/.autocompletion ; sleep 3\"";
+#elif _WIN32
+    cmdScript = "";
+#elif __APPLE__
+    cmdScript = "";
+#endif
     installApp(*ui->DesinstallationAutocompletion, *ui->InstallationAutocompletion, cmdScript,
                "background-color: #38a3a5; color: #000");
 }
 
 void MainWindow::on_OuvrirDooSearch_clicked()
 {
-    LaunchSystemApp->execute("xdg-open", QStringList() << "https://paulluxwaffle.synology.me/Doosearch/src/search.php" );
+    QString link = "https://paulluxwaffle.synology.me/Doosearch/src/search.php";
+    QDesktopServices::openUrl(QUrl(link));
 }
 
 void MainWindow::on_InstallationNoComprendo_clicked()
 {
+#ifdef __linux__
     cmdScript = "x-terminal-emulator --nofork -e sudo bash -c \"wget https://github.com/Heficience/HSuperMenu/raw/main/nocomprendo-without-qt-default.deb ; apt update ; apt install ./nocomprendo-without-qt-default.deb ; rm nocomprendo-without-qt-default.deb ; sleep 3\"";
+#elif _WIN32
+    cmdScript = "";
+#elif __APPLE__
+    cmdScript = "";
+#endif
     installApp(*ui->InstallationNoComprendo, *ui->DesinstallationNoComprendo, cmdScript,
                "background-color: #80ed99; color: #000");
 }
 
 void MainWindow::on_DesinstallationNoComprendo_clicked()
 {
+#ifdef __linux__
     cmdScript = "x-terminal-emulator --nofork -e sudo bash -c \"apt update ; apt remove nocomprendo ; sleep 3 \"";
+#elif _WIN32
+    cmdScript = "";
+#elif __APPLE__
+    cmdScript = "";
+#endif
     installApp(*ui->DesinstallationNoComprendo, *ui->InstallationNoComprendo, cmdScript,
                "background-color: #80ed99; color: #000");
 }
 
 void MainWindow::on_OuvrirSiteWeb_clicked() {
-    LaunchSystemApp->execute("xdg-open", QStringList() << "https://www.heficience.com/index.php" );
+    QString link = "https://www.heficience.com/index.php";
+    QDesktopServices::openUrl(QUrl(link));
 }
 
 void MainWindow::installApp(QPushButton &Install, QPushButton &Desintall, QString cmdSCriptUnique, QString Style) {
